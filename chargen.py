63f84b7142b80c74b2f39fb86a933cc5f5e51b55
Alex Mooney <amooney@covermymeds.com>
2017-01-20 23:28:58 -0500

    Optionally color the display
    - Terminal output will not color, regardless of the options.
    - Remove the non-color hint characters (+ and *)
    - Also, sort the characters based on their total stats.

--- chargen.py
+++ chargen.py
@@ -3,12 +3,15 @@ from random import randrange
 
 import click
 
-STR=0
-INT=1
-WIS=2
-DEX=3
-CON=4
-CHA=5
+STR = 0
+INT = 1
+WIS = 2
+DEX = 3
+CON = 4
+CHA = 5
+
+STATS = ['STR', 'INT', 'WIS', 'DEX', 'CON', 'CHA']
+
 
 def rollstat():
     return sum((randrange(6)+1 for x in range(3)))
@@ -18,7 +21,36 @@ def rollstats():
     return [rollstat() for stat in range(6)]
 
 
-def evalstats(stats):
+def stat_level(stat):
+    if stat > 17:
+        return 3
+    elif stat > 15:
+        return 2
+    elif stat > 12:
+        return 1
+    elif stat > 8:
+        return 0
+    elif stat > 5:
+        return -1
+    elif stat > 3:
+        return -2
+    return -3
+
+
+def stat_colors(stat, color=True):
+    if color:
+        return {-3: 'magenta',
+                -2: 'red',
+                -1: 'yellow',
+                0: 'white',
+                1: 'green',
+                2: 'blue',
+                3: 'cyan'
+                }[stat_level(stat)]
+    return ''
+
+
+def evalstats(stats, showall, color):
     classes = [
         {'name': 'Fighter', 'primes': [STR], 'reqs': []},
         {'name': 'Mage', 'primes': [INT], 'reqs': []},
@@ -53,53 +85,48 @@ def evalstats(stats):
         {'name': 'Witch', 'primes': [WIS, CHA], 'reqs': []},
         {'name': 'Ruinguard', 'primes': [STR, INT], 'reqs': [(INT, 9), (WIS, 9), (CHA, 9)]},
         ]
-    allowed_classes = ''
     for cls in classes:
         if cls == {}:
-            allowed_classes += '\n'
+            click.echo('')
             continue
 
         prime = min((stats[prime] for prime in cls['primes']))
 
-        if prime > 15:
-            fitness = 3
-        elif prime > 12:
-            fitness = 2
-        elif prime > 8:
-            fitness = 1
-        else:
-            fitness = 0
+        fitness = stat_level(prime)
 
         for req in cls['reqs']:
             if stats[req[0]] < req[1]:
-                fitness = 0
+                fitness = -1
+                prime = 3
                 break
 
-        if fitness == 0:
-            allowed_classes += ' '*len(cls['name']) + '  '
-        elif fitness == 1:
-            allowed_classes += cls['name'] + '  '
-        elif fitness == 2:
-            allowed_classes += cls['name'] + '+ '
-        elif fitness == 3:
-            allowed_classes += cls['name'] + '* '
-
-    click.echo(allowed_classes)
+        if fitness < 0:
+            if showall:
+                click.echo(''.join([c+'\u0336' for c in cls['name']]) + '  ', nl=False)
+        else:
+            click.echo(click.style(cls['name'] + '  ', fg=stat_colors(prime, color)), nl=False)
 
 
-def printstats(stats):
-    click.echo('STR:{:>3} INT:{:>3} WIS:{:>3} DEX:{:>3} CON:{:>3} CHA:{:>3}'.format(*stats))
-    evalstats(stats)
-    click.echo('')
+def printstats(stats, showall, color):
+    for i, stat in enumerate(stats):
+        click.echo(click.style('{}:{:>3}  '.format(STATS[i], stat), fg=stat_colors(stat, color)), nl=False)
+    total_level = 2*sum(stat_level(s) for s in stats) + 10
+    click.echo(click.style('  Total:{:>3}'.format(sum(stats)), fg=stat_colors(total_level, color)))
+
+    evalstats(stats, showall, color)
+    click.echo('\n')
 
 
 @click.command()
-@click.option('-c', '--count', default=1, help='Number of characters.')
-def generate(count):
+@click.option('-n', '--number', default=5, help='Number of characters (default 5).')
+@click.option('--showall/--no-showall', default=False, help='Show classes not allowed by stats.')
+@click.option('--color/--no-color', default=True, help='Show class affinities in color.')
+def generate(number, showall, color):
     """Character roller for ACKS."""
-    for x in range(count):
-        stats = rollstats()
-        printstats(stats)
+    statss = sorted([rollstats() for i in range(number)], key=lambda arr: -sum(arr))
+    for stats in statss:
+        printstats(stats, showall, color)
+
 
 if __name__ == '__main__':
     generate()
