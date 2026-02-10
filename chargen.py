cb1d53e2cd369d079a2c5ee267f138bf8ff4caf2
Alex Mooney <alexmmooney@gmail.com>
2026-02-10 11:20:59 -0800

    Get rid of the classes in chargen
    They're from ACKS I and I never use them anyway.

--- chargen.py
+++ chargen.py
@@ -62,115 +62,6 @@ def stat_colors(stat, opts):
     return ''
 
 
-def evalstats(stats, opts):
-    classes = [
-        {'section': 'Human Classes'},
-        {'name': 'Fighter', 'primes': [STR], 'reqs': []},
-        {'name': 'Mage', 'primes': [INT], 'reqs': []},
-        {'name': 'Cleric', 'primes': [WIL], 'reqs': []},
-        {'name': 'Thief', 'primes': [DEX], 'reqs': []},
-        {'name': 'Assassin', 'primes': [STR, DEX], 'reqs': []},
-        {'name': 'Bard', 'primes': [CHA, DEX], 'reqs': []},
-        {'name': 'Bladedancer', 'primes': [WIL, DEX], 'reqs': []},
-        {'name': 'Explorer', 'primes': [STR, DEX], 'reqs': []},
-        {'name': 'Anti-paladin', 'primes': [STR, CHA], 'reqs': []},
-        {'name': 'Barbarian', 'primes': [STR, CON], 'reqs': []},
-        {'name': 'Mystic', 'primes': [WIL, DEX, CON, CHA], 'reqs': []},
-        {'name': 'Paladin', 'primes': [STR, CHA], 'reqs': []},
-        {'name': 'Priestess', 'primes': [WIL, CHA], 'reqs': []},
-        {'name': 'Shaman', 'primes': [WIL], 'reqs': []},
-        {'name': 'Venturer', 'primes': [CHA], 'reqs': []},
-        {'name': 'Warlock', 'primes': [INT], 'reqs': []},
-        {'name': 'Witch', 'primes': [WIL, CHA], 'reqs': []},
-        {'section': 'Dwarven Classes'},
-        {'name': 'Vaultguard', 'primes': [STR], 'reqs': [(CON, 9)]},
-        {'name': 'Craftpriest', 'primes': [WIL], 'reqs': [(CON, 9)]},
-        {'name': 'Delver', 'primes': [DEX], 'reqs': [(CON, 9)]},
-        {'name': 'Fury', 'primes': [STR], 'reqs': [(CON, 9)]},
-        {'name': 'Machinist', 'primes': [INT, DEX], 'reqs': [(CON, 9)]},
-        {'section': 'Elven Classes'},
-        {'name': 'Spellsword', 'primes': [STR, INT], 'reqs': []},
-        {'name': 'Nightblade', 'primes': [DEX, INT], 'reqs': []},
-        {'name': 'Courtier', 'primes': [INT, CHA], 'reqs': [(INT, 9)]},
-        {'name': 'Enchanter', 'primes': [INT, CHA], 'reqs': [(INT, 9)]},
-        {'name': 'Ranger', 'primes': [STR, DEX], 'reqs': [(INT, 9)]},
-        {'section': 'Other demi-humans'},
-        {'name': 'Gnomish Trickster', 'primes': [CON, CHA],
-            'reqs': [(CON, 9), (INT, 9)]},
-        {'name': 'Nobrian Wonderworker', 'primes': [INT, WIL],
-            'reqs': [(i, 11) for i in range(6)]},
-        {'name': 'Thrassian Gladiator', 'primes': [STR],
-            'reqs': [(STR, 9), (DEX, 9), (CON, 9)]},
-        {'name': 'Zaharan Ruinguard', 'primes': [STR, INT],
-            'reqs': [(INT, 9), (WIL, 9), (CHA, 9)]},
-        {'section': 'Heroic Classes'},
-        {'name': 'Beastmaster', 'primes': [STR, DEX, CON, CHA], 'reqs': []},
-        {'name': 'Berserker', 'primes': [STR, CON], 'reqs': []},
-        {'name': 'Chosen', 'primes': [i for i in range(6)],
-            'reqs': [(i, 9) for i in range(6)], 'special': 'chosen'},
-        {'name': 'Ecclesiastic', 'primes': [WIL], 'reqs': []},
-        {'name': 'Elven Spellsinger', 'primes': [INT, CHA], 'reqs': [(INT, 9)]},
-        {'name': 'Freebooter Expeditionary', 'primes': [DEX, WIL], 'reqs': []},
-        {'name': 'Freebooter Ruffian', 'primes': [DEX, STR], 'reqs': []},
-        {'name': 'Freebooter Scoundrel', 'primes': [DEX, CHA], 'reqs': []},
-        {'name': 'Freebooter Wayfarer', 'primes': [DEX, CON], 'reqs': []},
-        {'name': 'Halfling Bounder', 'primes': [STR, DEX], 'reqs': [(DEX, 9)]},
-        {'name': 'Halfling Burglar', 'primes': [DEX], 'reqs': [(DEX, 9)]},
-        {'name': 'Loremaster', 'primes': [INT, WIL], 'reqs': []},
-        {'name': 'Nobrian Champion', 'primes': [STR, CHA],
-            'reqs': [(i, 11) for i in range(6)]},
-        {'name': 'Nobrian Wizard', 'primes': [INT, WIL],
-            'reqs': [(i, 11) for i in range(6)]},
-        {'name': 'Occultist', 'primes': [INT, WIL], 'reqs': []},
-        {'name': 'Rune Maker', 'primes': [STR, WIL], 'reqs': []},
-        {'name': 'Thrassian Deathchanter', 'primes': [STR, INT, CHA],
-            'reqs': [(STR, 9), (DEX, 9), (CON, 9)]},
-        {'name': 'Venturer', 'primes': [CHA], 'reqs': []},
-        {'name': 'Warmistress', 'primes': [DEX, CHA], 'reqs': [(STR, 9)]},
-        {'name': 'Zaharan Darklord', 'primes': [INT, CHA],
-            'reqs': [(INT, 9), (WIL, 9), (CHA, 9)]},
-        {'name': 'Zaharan Sorcerer', 'primes': [INT],
-            'reqs': [(INT, 9), (WIL, 9), (CHA, 9)]},
-        ]
-    section = ''
-    for cls in classes:
-        new_section = cls.get('section')
-        if new_section:
-            if opts['showall']:
-                click.echo('\n' + new_section)
-            else:
-                section = new_section
-            continue
-
-        prime = min((stats[prime] for prime in cls['primes']))
-
-        fitness = stat_level(prime)
-
-        for req in cls['reqs']:
-            if stats[req[0]] < req[1]:
-                fitness = -1
-                prime = 3
-                break
-
-        if cls.get('special') == 'chosen':
-            if max(stats) < 18:
-                fitness = -1
-                prime = 3
-                break
-
-        if fitness < 0:
-            if opts['showall']:
-                click.echo(''.join([c+'\u0336' for c in cls['name']]) + '  ',
-                           nl=False)
-        else:
-            if section:
-                click.echo('\n    ' + section)
-                section = None
-            click.echo(click.style(cls['name'] + '  ',
-                                   fg=stat_colors(prime, opts)),
-                       nl=False)
-
-
 def printstats(stats, opts):
     for i, stat in enumerate(stats):
         click.echo(click.style('{}:{:>3}  '.format(STATS[i], stat),
@@ -181,15 +72,9 @@ def printstats(stats, opts):
     else:
         click.echo('')
 
-    if opts['show_classes']:
-        evalstats(stats, opts)
-        click.echo('\n')
-
 
 @click.command()
 @click.option('-n', '--number', default=5, help='Number of characters (default 5).')
-@click.option('--classes', is_flag=True, help="Display the list of classes qualified for.")
-@click.option('--showall', is_flag=True, help='Show classes not allowed by stats.')
 @click.option('--no-color', is_flag=True, help='Do not use color to print.')
 @click.option('--no-sort', is_flag=True, help='Do not sort the characters by total stats.')
 @click.option('--no-gold', is_flag=True, help='Do not roll for gold.')
@@ -197,12 +82,10 @@ def printstats(stats, opts):
 @click.option('-p', '--primaries', default=None, help='Primary attributes (comma delimited) via rolling 5d6 min 13.')
 @click.option('-b', '--boosts', default=None, help='Boost attributes (comma delimited) via rolling 4d6 min 9.')
 @click.option('--seed', default=None, help='Override the RNG seed.')
-def generate(number, classes, showall, no_color, no_sort, no_gold, heroic, primaries, boosts, seed):
+def generate(number, no_color, no_sort, no_gold, heroic, primaries, boosts, seed):
     """Character generator for ACKS."""
     opts = {
         'number': number,
-        'show_classes': classes,
-        'showall': showall,
         'color': not no_color,
         'sort': not no_sort,
         'gold': not no_gold,
